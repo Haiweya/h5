f2b42b64ef062a228afc53aa9c0fb6a63abe35f8
Haiweya <mailika888@sina.com>
2021-05-14 15:39:01 +0800

    初始化vant项目

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <router-view />
+        <van-tabbar route>
+            <van-tabbar-item replace to="/" icon="home-o">首页</van-tabbar-item>
+            <van-tabbar-item replace to="/about" icon="orders-o">介绍</van-tabbar-item>
+        </van-tabbar>
     </div>
-    <router-view/>
-  </div>
 </template>
+<script>
+import { Tabbar, TabbarItem } from 'vant';
+export default {
+    components: {
+        [Tabbar.name]: Tabbar,
+        [TabbarItem.name]: TabbarItem,
+    },
+}
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {
+    Lazyload
+} from 'vant'
 
+// vant
+Vue.use(Lazyload)
 Vue.config.productionTip = false
 
+// rem
+import "lib-flexible/flexible"
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
