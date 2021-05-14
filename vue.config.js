d913d1274175c9277715995b50b7169606a8d8a4
Haiweya <mailika888@sina.com>
2021-05-14 15:44:01 +0800

    配置多环境变量

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,23 @@
+console.log(process.env.NODE_ENV);
+const port = process.env.port || 9999;
+
 module.exports = {
-    lintOnSave: false
+    // 是否在开发环境下通过 eslint-loader 在每次保存时 lint 代码 
+    // (在生产构建时禁用 eslint-loader) 应该是!==,这里设置为===是为了不进行检查
+    lintOnSave: process.env.NODE_ENV === 'production',
+    devServer: {
+        port,
+        open: true,
+        // 前端应用和后端 API 服务器没有运行在同一个主机上，
+        // 你需要在开发环境下将 API 请求代理到 API 服务器
+        proxy: {
+            [process.env.VUE_APP_BASE_API]: {
+                target: "http://localhost:8000/mock", // 接口的域名
+                changeOrigin: true, // 开启代理，在本地创建一个虚拟服务端
+                pathRewrite: {
+                    [process.env.VUE_APP_BASE_API]: ""
+                }
+            }
+        }
+    }
 }
